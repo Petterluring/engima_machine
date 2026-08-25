427e6ba3f8d3fd5bbf89087c7f739bf96d21490b
Petter Gustafsson <petter.gustafsson1998@gmail.com>
2026-08-25 08:26:13 +0200

    - Added configuration models for Rotor, Rotors, Plugboard, and EnigmaMachine classes.

--- src/enigma_machine/config/machine.py
+++ src/enigma_machine/config/machine.py
@@ -1,1 +1,27 @@
 """Module containing EnigmaMachineConfig."""
+
+
+from pathlib import Path
+
+from pydantic import BaseModel
+
+from .plugboard import PlugboardConfig
+from .rotor import RotorsConfig
+
+
+class EnigmaMachineConfig(BaseModel):
+    """Pydantic model for EnigmaMachine class.
+
+    Attributes:
+        rotors: RotorsConfig       - Rotor set to use.
+        reflector: str             - See Reflector class in reflector package.
+        plugboard: PlugboardConfig - Plugboard to use.
+    """
+    rotors: RotorsConfig
+    reflector: str
+    plugboard: PlugboardConfig
+
+    @classmethod
+    def from_yaml(cls, path: str | Path) -> None:
+        """Load configuration from a yaml file."""
+        pass
