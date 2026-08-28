c02926b0f23a8cd85d3d75be40f847ffdbf4161c
Petter Gustafsson <petter.gustafsson1998@gmail.com>
2026-08-28 10:13:07 +0200

    Working on docs....

--- src/enigma_machine/reflector/reflector.py
+++ src/enigma_machine/reflector/reflector.py
@@ -9,19 +9,18 @@ class Reflector:
     The reflector wires alphabetic letters in unique pairs with bidirectional encoding.
     A unique pair means that two letters in a pair cannot be found in a different pair.
 
-    The wiring is defined by a permutation of a supported alphabet where the first half of the
-    permutation is wired/mapped to the second half. For instance, the permutation QWZJTYRLPFNSVXCHAMOEGKUBID
-    would be wired as follows:
+    The wiring is defined by a permutation of an alphabetic set of letters where the first half
+    is wired/mapped with the second half. For instance, the permutation QWZJTYRLPFNSVXCHAMOEGKUBID
+    is wired as follows:
                         QWZJTYRLPFNSV
                         XCHAMOEGKUBID
-
-    See alphabet.py for supported alphabets.
+    See Alphabet enum for supported letter sets.
     """
     def __init__(self, wiring: str) -> None:
         """Class initializer.
 
         Args:
-            wiring: str - A wiring of a supported alphabet.
+            wiring: str - Permutation of an alphabetic set of letters defining the wiring of the reflector.
         """
         alphabet, norm_wiring = Alphabet.infer_alphabet_and_normalize(wiring)
 
@@ -29,17 +28,13 @@ class Reflector:
         self._wiring = norm_wiring
 
     def encode(self, alph_letter: str, normalize: bool = True) -> str:
-        """Return the letter that is wired to 'alph_letter'.
+        """Return the letter wired to 'alph_letter'.
 
-        For instance, if the input letter A is wired to C, return C.
+        For instance, if input letter A is wired to C, return C.
 
         Args:
-            alph_letter: str - Alphabetic letter to encode.
-            normalize: bool  - Flag for normalizing input letter.
-
-        Returns:
-            str - String representing the encoded letter.
-
+            alph_letter: Alphabetic letter to encode.
+            normalize:   Flag stating if input should be normalized before encoded.
         """
         alph_letter = self._alphabet.normalize(alph_letter) if normalize else alph_letter
         divider = len(self._wiring) // 2
@@ -48,10 +43,9 @@ class Reflector:
 
     @property
     def permutation(self) -> str:
-        """Return the wiring of the alphabet."""
+        """Return the wiring."""
         return "".join(self.encode(letter) for letter in self._alphabet.value)
 
     @property
-    def alphabet(self) -> Alphabet:
-        """Return the alphabet."""
+    def alphabet(self) -> Alphabet:  # noqa: D102
         return self._alphabet
